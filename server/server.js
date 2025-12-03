b98837be23af2b489b39b891662288aeb8199f94
atfdark <147408184+atfdark@users.noreply.github.com>
2025-12-03 21:05:33 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
@@ -15,6 +16,7 @@ const io = socketIo(server, {
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..')));
 
 // Store connected clients
 const connectedClients = new Map();
